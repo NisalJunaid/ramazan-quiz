701b2183a82d9f8a3e98b917346fa6da7631746f
NisalJunaid <105519086+NisalJunaid@users.noreply.github.com>
2026-01-30 15:13:04 +0500

    Add quiz and admin routes with controllers

---
+++ app/Http/Controllers/AttemptController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Response;
+
+class AttemptController extends Controller
+{
+    public function submitAttempt(): Response
+    {
+        return response('Not implemented.', 501);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,11 @@
 <?php
 
+use App\Http\Controllers\AdminChoiceController;
+use App\Http\Controllers\AdminQuestionController;
+use App\Http\Controllers\AdminQuizController;
+use App\Http\Controllers\AttemptController;
+use App\Http\Controllers\LeaderboardController;
+use App\Http\Controllers\QuizController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +19,20 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('home');
+Route::view('/', 'home')->name('home');
+Route::get('/leaderboard', [LeaderboardController::class, 'todayLeaderboard'])->name('leaderboard');
+
+Route::middleware(['auth', 'not_banned'])->group(function () {
+    Route::get('/quiz/today', [QuizController::class, 'showTodayQuiz'])->name('quiz.today');
+    Route::post('/quiz/{quizDay}/start', [QuizController::class, 'startAttempt'])->name('quiz.start');
+    Route::post('/attempt/{attempt}/submit', [AttemptController::class, 'submitAttempt'])->name('attempt.submit');
+});
+
+Route::middleware(['auth', 'admin'])->group(function () {
+    Route::get('/admin', [AdminQuizController::class, 'index'])->name('admin.dashboard');
+    Route::get('/admin/quizzes', [AdminQuizController::class, 'index'])->name('admin.quizzes.index');
+    Route::post('/admin/quizzes', [AdminQuizController::class, 'store'])->name('admin.quizzes.store');
+    Route::put('/admin/quizzes/{id}', [AdminQuizController::class, 'update'])->name('admin.quizzes.update');
+    Route::post('/admin/questions', [AdminQuestionController::class, 'store'])->name('admin.questions.store');
+    Route::post('/admin/choices', [AdminChoiceController::class, 'store'])->name('admin.choices.store');
 });
